b0a6a5c4b090d73caaf539a0f4eccff61a823f7f
marttcw <15316579+marttcw@users.noreply.github.com>
2019-09-08 00:26:05 +0100

    remove cpr

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
+/*
+ *
+ */
+// https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid=244630
+
 #include <iostream>
 
 int main(int argc, char **argv)
 {
-  std::cout << "Help\n";
+  std::cout << "TEST\n";
+
   return 0;
 }
 
