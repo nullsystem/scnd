a52087087e0d907b725f4e24d3498774329e6c22
marttcw <15316579+marttcw@users.noreply.github.com>
2019-09-09 01:10:22 +0100

    options setup 1

--- src/wrapper/curl.cpp
+++ src/wrapper/curl.cpp
@@ -8,15 +8,12 @@ std::size_t wrapper::curl::callback(const char *in, std::size_t size, std::size_
   return totalBytes;
 }
 
-wrapper::curl::curl(std::string_view newUrl, int timeout)
+wrapper::curl::curl()
 {
-  this->url = newUrl;
   this->httpData = std::make_unique<std::string>();
 
   this->curlPointer = curl_easy_init();
-  curl_easy_setopt(this->curlPointer, CURLOPT_URL, this->url.data());
   curl_easy_setopt(this->curlPointer, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-  curl_easy_setopt(this->curlPointer, CURLOPT_TIMEOUT, timeout);
   curl_easy_setopt(this->curlPointer, CURLOPT_FOLLOWLOCATION, 1L);
   curl_easy_setopt(this->curlPointer, CURLOPT_WRITEFUNCTION, wrapper::curl::callback);
   curl_easy_setopt(this->curlPointer, CURLOPT_WRITEDATA, httpData.get());
@@ -31,6 +28,17 @@ void wrapper::curl::perform()
 {
   curl_easy_perform(this->curlPointer);
   curl_easy_getinfo(this->curlPointer, CURLINFO_RESPONSE_CODE, &this->httpResponseCode);
+}
+
+void wrapper::curl::setUrl(std::string_view newUrl)
+{
+  this->url = newUrl;
+  curl_easy_setopt(this->curlPointer, CURLOPT_URL, this->url.data());
+}
+
+void wrapper::curl::setTimeout(int timeout)
+{
+  curl_easy_setopt(this->curlPointer, CURLOPT_TIMEOUT, timeout);
 }
 
 int wrapper::curl::getHttpResponseCode()
