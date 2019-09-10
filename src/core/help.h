8a1994eaa9503d2c1d992fca4e8c15e15ef4b752
marttcw <15316579+marttcw@users.noreply.github.com>
2019-09-10 11:57:23 +0100

    renamed to steamcountsnotifyd

--- src/core/help.h
+++ src/core/help.h
@@ -2,7 +2,7 @@
 #define CORE__HELP_H
 
 #define HELP \
-  "neonotifytokyo help\n"\
+  "steamcountsnotifyd help\n"\
   "\n"\
   "-i   Interval: How long in minutes to delay between each player count fetch.\n"\
   "-t   Timeout:  How long in seconds the notification stays shown.\n"\
@@ -12,7 +12,7 @@
   "-h   Prints this help information.\n"
 
 #define VERSION \
-  "neonotifytokyo\n"\
+  "steamcountsnotifyd\n"\
   "The Steam player count notification daemon\n"\
   "\n"\
   "Version Pre-Alpha 2019-09-08\n"
