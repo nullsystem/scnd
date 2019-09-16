ccf6228419ab2e4d414c0e0b7f2f2411502ba6ea
martin <mtcw@disroot.org>
2019-09-16 12:52:34 +0100

    notification no longer init everytime and bit of callback starter

--- src/wrapper/notify.cpp
+++ src/wrapper/notify.cpp
@@ -7,6 +7,8 @@ wrapper::notify::notify(std::string_view summary, std::string_view body, std::st
 
 wrapper::notify::~notify()
 {
+  notify_notification_clear_actions(this->notification);
+  g_object_unref(G_OBJECT(this->notification));
 }
 
 void wrapper::notify::init(std::string_view threadName)
@@ -29,7 +31,12 @@ void wrapper::notify::setTimeout(unsigned int timeout)
   notify_notification_set_timeout(this->notification, timeout);
 }
 
-void wrapper::notify::unref()
+void wrapper::notify::addAction(std::string_view action, std::string_view label, NotifyActionCallback callback, gpointer user_data, GFreeFunc free_func)
 {
-  g_object_unref(G_OBJECT(this->notification));
+  notify_notification_add_action(this->notification, action.data(), label.data(), NOTIFY_ACTION_CALLBACK(callback), user_data, free_func);
+}
+
+void wrapper::notify::update(std::string_view summary, std::string_view body, std::string_view icon)
+{
+  notify_notification_update(this->notification, summary.data(), body.data(), icon.data());
 }
