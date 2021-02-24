f905cc2593e9361e8fbb14e2e4c4c7c3e746ce38
marttcw <mtcw@disroot.org>
2021-02-24 12:47:43 +0000

    Async download and run of each appid works
    (Tokio) async spawn used to execute getting counter player onlines.

--- src/game.rs
+++ src/game.rs
@@ -1,37 +1,54 @@
 use crate::{config, notify};
 use std::{thread, time};
-
-pub async fn req_then_notify(game: &config::ConfigGame, notify_timeout: u32) -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get(&format!(
-        "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}",
-        appid = game.appid
-    )).await?;
-
-    let resp_data = resp.text().await?;
-    let resp_json: serde_json::Value = serde_json::from_str(&resp_data)?;
-    let count = resp_json["response"]["player_count"].clone();
-
-    match notify::new(
-        &game.name,
-        &format!("{} - {} Players Online", game.name, count),
-        notify_timeout,
-    ) {
-        Err(why) => println!("{} - {}: cannot notify: {}", game.name, game.appid, why),
-        Ok(_) => (),
-    }
-
-    Ok(())
-}
+use tokio::task;
+use futures::future::join_all;
 
 pub async fn main_loop(cfg: &config::Config) {
+    let notify_timeout = cfg.notify_timeout.clone();
+    let interval = cfg.interval.clone();
+
     loop {
+        let mut tasks: Vec<task::JoinHandle<()>> = vec![];
+
         for game in &cfg.games {
-            match req_then_notify(&game, cfg.notify_timeout).await {
-                Err(why) => println!("ERROR: {}: {}", &game.name, why),
-                Ok(_) => (),
-            };
+            let game = game.clone();
+            let url = format!(
+                    "https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?appid={appid}",
+                    appid = game.appid
+                );
+
+            tasks.push(tokio::spawn(async move {
+                match reqwest::get(&url).await {
+                    Ok(resp) => {
+                        match resp.text().await {
+                            Ok(text) => {
+                                match serde_json::from_str(&text) {
+                                    Ok(text_json) => {
+                                        let text_json: serde_json::Value = text_json;
+                                        let count = text_json["response"]["player_count"].clone();
+
+                                        match notify::new(
+                                            &game.name,
+                                            &format!("{} - {} Players Online", game.name, count),
+                                            notify_timeout,
+                                        ) {
+                                            Err(why) => println!("{} - {}: cannot notify: {}", game.name, game.appid, why),
+                                            Ok(_) => (),
+                                        }
+                                        thread::sleep(time::Duration::from_secs((60 * interval) as u64));
+                                    }
+                                    Err(why) => println!("ERROR: Cannot extract json from text - {}", why),
+                                }
+                            }
+                            Err(why) => println!("ERROR: Cannot get the response text - {}", why),
+                        }
+                    }
+                    Err(why) => println!("ERROR: Cannot download appid '{}' - {}", game.appid, why),
+                }
+            }));
         }
-        thread::sleep(time::Duration::from_secs(60 * cfg.interval as u64));
+
+        join_all(tasks).await;
     }
 }
 
