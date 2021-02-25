5dbe6a0eda34c9730b8af6a441d9d5609093717c
marttcw <mtcw@disroot.org>
2021-02-25 14:06:41 +0000

    Timeout, action type, and (semi-working) daemon
    NEW: Timeout now applied to GET request
    NEW: Can have different action type: 0 for click, 1 for button
    NEW/FIX: Timeout properly used in seconds unit
    NEW/TODO: Daemon works, however ISSUE: counter not working

--- src/notify.rs
+++ src/notify.rs
@@ -1,23 +1,29 @@
 use notify_rust::{Notification, Hint};
 use std::process::Command;
 use std::thread;
+use crate::config::ActionType;
 
-pub async fn new(summary: &str, body: &str, timeout: u32, appid: u32) -> Result<(), Box<dyn std::error::Error>> {
+pub async fn new(summary: &str, body: &str, timeout: u32, appid: u32, action_type: ActionType) -> Result<(), Box<dyn std::error::Error>> {
     let summary: String = summary.into();
     let body: String = body.into();
 
     thread::spawn(move || {
-        Notification::new()
-            .summary(&summary)
+        let mut notify = Notification::new();
+        notify.summary(&summary)
             .body(&body)
-            .action("run", "Launch game")
             .hint(Hint::Resident(true))
-            .timeout(timeout as i32)
-            .show()
+            .timeout((timeout * 1000) as i32);
+
+        let notify = match action_type {
+            ActionType::Button => notify.action("run", "Launch game"),
+            ActionType::Click => notify.action("default", "default"),
+        };
+
+        notify.show()
             .unwrap()
             .wait_for_action({|action|
                 match action {
-                    "run" => {
+                    "run" | "default" => {
                         thread::spawn(move || {
                             Command::new("steam")
                                 .arg(format!("steam://rungameid/{}", appid))
@@ -32,5 +38,14 @@ pub async fn new(summary: &str, body: &str, timeout: u32, appid: u32) -> Result<
     });
 
     Ok(())
+}
+
+pub fn daemon_startup() {
+    Notification::new()
+        .summary("steamcountsnotifyd")
+        .body("steamcountsnotifyd daemon started up")
+        .timeout(5000)
+        .show()
+        .unwrap();
 }
 
